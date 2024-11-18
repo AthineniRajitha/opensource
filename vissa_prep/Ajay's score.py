6dde071e240ce5cfc18b70b95a348a3b6d91f323
AthineniRajitha <147744341+AthineniRajitha@users.noreply.github.com>
2024-11-18 10:25:00 +0530

    Create Ajay's score.py

---
+++ vissa_prep/Ajay's score.py
@@ -0,0 +1,9 @@
+t=int(input().strip())
+for _ in range(t):
+    try:
+        x,n=map(int, input().split())
+        points_test_case=x//10
+        score=points_test_case*n
+        print(score)
+    except ValueError:
+        print("Invalid Test Case")
